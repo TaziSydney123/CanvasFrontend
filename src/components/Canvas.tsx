41ae2fb17e8d06e0082175f3735216762393f7e4
TaziSydney123 <tazisydney@gmail.com>
2024-03-15 17:27:34 -0400

    Made canvas look good

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,32 +1,60 @@
 import { IonCol, IonGrid, IonRow } from '@ionic/react';
-import { ReactNode, useState } from 'react';
 
 type CanvasProps = {
   colorData: string[][];
 };
 
-export default function Canvas(props: CanvasProps) {
+export default function Canvas(props: Readonly<CanvasProps>) {
   const { colorData } = props;
   return (
-    <IonGrid fixed>
-      {colorData.map(row => (
-        <IonRow>
-          {row.map(color => (
-            <Pixel color={color}></Pixel>
-          ))}
-        </IonRow>
-      ))}
+    <IonGrid fixed className="canvas-grid">
+      {colorData.map((row, rowIndex) => {
+        const edgeRow = rowIndex == 0 || rowIndex == colorData.length - 1;
+        return (
+          <IonRow key={row.join("")}>
+            {row.map((color, colIndex) => {
+              const edgeCol = colIndex == 0 || colIndex == colorData[rowIndex].length - 1;
+              return (
+                <Pixel color={color} corner={edgeRow && edgeCol ? {row: rowIndex, col: colIndex} : null} key={rowIndex.toString() + colIndex.toString()} />
+              );
+            })}
+          </IonRow>
+        );
+      })}
     </IonGrid>
   );
 };
 
-type PixelProps = {
-  color: string,
+type CornerPosition = {
+  row: number,
+  col: number
 };
 
-function Pixel(props: PixelProps) {
-  const { color } = props;
+type PixelProps = {
+  color: string,
+  corner: CornerPosition | null,
+};
+
+function Pixel(props: Readonly<PixelProps>) {
+  const { color, corner } = props;
+  console.log(getCornerStyle());
   return (
-    <IonCol className="canvas-pixel" style={{backgroundColor: color}} />
+    <IonCol className="canvas-pixel" style={{backgroundColor: color, ...getCornerStyle()}} />
   );
+
+  function getCornerStyle() {
+    if (!corner) {
+      return {};
+    }
+    if (corner.row > 0) {
+      if (corner.col > 0) {
+        return {borderBottomRightRadius: 8};
+      }
+      return {borderBottomLeftRadius: 8};
+    }
+    if (corner.col > 0) {
+      return {borderTopRightRadius: 8};
+    }
+    return {borderTopLeftRadius: 8};
+  }
 }
